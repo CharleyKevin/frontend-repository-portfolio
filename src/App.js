b8a7b6f11669677a34170403251fd407cec9bf2b
Charley Kevin <charleykevin@protonmail.ch>
2020-04-19 12:38:54 -0300

    crud, add button like and create form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const title = document.getElementById('newTitle').value;
+    const url = document.getElementById('newUrl').value;
+    const tecnology1 = document.getElementById('newTechnology1').value;
+    const tecnology2 = document.getElementById('newTechnology2').value;
+    const tecnology3 = document.getElementById('newTechnology3').value;
+
+    const techs = [];
+    if(tecnology1 !== 0) techs.push(tecnology1);
+    if(tecnology2 !== 0) techs.push(tecnology2);
+    if(tecnology3 !== 0) techs.push(tecnology3);
+
+    console.log(techs);
+
+    const repository = {
+      title: title,
+      url: url,
+      techs:techs
+    };
+
+    const response = await api.post('/repositories', repository);
+
+    const repositoryResponse = response.data;
+
+    setRepositories([ ...repositories, repositoryResponse]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if(repositoryIndex < 0) {
+      alert('Elemento não encontrado');
+    }
+ 
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
+  }
+
+  async function handleAddLikeRepository(id){    
+    const  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    const response = await api.post(`/repositories/${id}/like`);
+
+    repositories[repositoryIndex]["likes"] = response.data.likes;
+
+    setRepositories([ ...repositories ]);    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repositories.map(repository => 
+              <li key={repository.id}>
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+                <button onClick={() => handleAddLikeRepository(repository.id)}>
+                  Like {repository.likes}
+                </button>{ repository.title}
+                </li>)
+          }   
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <div className="form">
+        <label>Título</label>
+        <input className="input" type="text" id="newTitle"/><br/>
+        <label>Url</label>
+        <input className="input" type="text" id="newUrl"/><br/>
+        <label>Tecnologia 1</label>
+        <input className="input" type="text" id="newTechnology1"/><br/>
+        <label>Tecnologia 2</label>
+        <input className="input" type="text" id="newTechnology2"/><br/>
+        <label>Tecnologia 3</label>
+        <input className="input" type="text" id="newTechnology3"/><br/>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
